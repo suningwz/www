7feaae5c9a0c50443b05a6027789c8de17ae9dc0
suningwz <2437526685@qq.com>
2021-03-21 21:57:24 +0800

    model的defaults属性和initialize方法

--- ninghao/app.js
+++ ninghao/app.js
@@ -2,4 +2,12 @@
 * 模型 - Model
 */
 
-var Note = Backbone.Model.extend({});
+var Note = Backbone.Model.extend({
+    defaults:{
+        title:'',
+        created_at:new Date()
+    },
+    initialize:function(){
+        console.log('新建了一条笔记：'+this.get('title'));
+    }
+});
