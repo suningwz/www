54c2aa96f9437fad33fc5cbe3ebb55e6eef04147
suningwz <2437526685@qq.com>
2021-03-21 22:05:33 +0800

    model属性发生变化触发change事件函数

--- ninghao/app.js
+++ ninghao/app.js
@@ -9,5 +9,12 @@ var Note = Backbone.Model.extend({
     },
     initialize:function(){
         console.log('新建了一条笔记：'+this.get('title'));
+        this.on('change',function(model,options){
+            console.log('属性发生了变化');
+        });
+        this.on('change title',function(model,options){
+            console.log('title发生了变化');
+        });
+
     }
 });
